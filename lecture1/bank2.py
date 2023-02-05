7eb2a92ae6fe579d54f80bc767e4cf79014d0155
badr <bernardoantoniod@gmail.com>
2023-02-05 19:24:22 -0300

    cambien la solucion en bank2 utilizando una funcion

--- lecture1/bank2.py
+++ lecture1/bank2.py
@@ -1,8 +1,14 @@
-g = input("Greeting: ").strip().casefold()
+def main():
 
-if g.startswith("hello"):
-    print("$0")
-elif g.startswith("h"):
-    print("$20")
-else: print("$100")
+    g = input("Greeting: ").strip().casefold()
+    promises_check(g)
 
+def promises_check(g):
+
+    if g.startswith("hello"):
+        print("$0")
+    elif g.startswith("h"):
+        print("$20")
+    else: print("$100")
+
+main()
