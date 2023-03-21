90b7a85b5d8247f3ab7504f252d50276f6f999f9
badr <bernardoantoniod@gmail.com>
2023-03-21 20:52:02 -0300

    termine figlet por mi cuenta yujuuu

--- lecture4/figlet.py
+++ lecture4/figlet.py
@@ -6,11 +6,22 @@ figlet = Figlet()
 figlet.getFonts()
 
 # to set a font # Where the name of the fonts are in a link  and has to be in ""#
-figlet.setFont(font="banner")
+#### figlet.setFont(font="banner") #### donde banner es un tipo de letra#
 
 import random
 import sys
 
-text = input("Input: ")
+if len(sys.argv) == 1:
+    text_in = input("Input: ")
+    random_font = random.choice(figlet.getFonts())
+    figlet.setFont(font=str(random_font))
+    print(figlet.renderText(text_in)) 
 
-print(text, sys.argv[1])
+elif len(sys.argv) == 3 and sys.argv[1] == "--font" or sys.argv[1] == "-f":
+    if sys.argv[2] in figlet.getFonts():
+        text_in = input("Input: ")
+        figlet.setFont(font=str(sys.argv[2]))
+        print(figlet.renderText(text_in))
+    else: sys.exit("Invalid usage")
+else:
+    sys.exit("Invalid usage")
