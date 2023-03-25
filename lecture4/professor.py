0337fa38f1efc6fad8f7df5fda27c3f69e572eef
badr <bernardoantoniod@gmail.com>
2023-03-25 19:24:47 -0300

    vi tutorial para resolver professor completo

--- lecture4/professor.py
+++ lecture4/professor.py
@@ -4,56 +4,57 @@ def main():
 
     level = get_level()
 
-    i = 1
+    i_times = 1
     score = 0
-    while i <= 10:
+
+    while i_times <= 10:
         x = generate_integer(level)
         y = generate_integer(level)
-        print(x, " + ", y, " = ", end="")
-        users_answer = int(input())
-        answer = x+y
-        if users_answer == answer:
+        user_answer = simulate_attemps(x,y)
+        if user_answer == True:
             score += 1
-        elif users_answer != answer:
-            print("EEE")
-            attemps = 2
-            while users_answer != answer:
-                while attemps <= 3:
-                    print(x, " + ", y, " = ", end="")
-                    users_answer = int(input())
-                    if users_answer == answer:
-                        score += 1
-                        break
-                    else:
-                        attemps += 1
-                    print("EEE")
-                print(x, " + ", y, " = ",answer)
-                break
-        i += 1
+        i_times += 1
 
-    print(score)
+    print(f"Score: {score}")
 
 def get_level():
-    while True:
-        try:     
-            level = int(input("Level: "))
-            if level > 0 and level <= 3:
-                return level
-        except ValueError:
-            pass
-
+    while True:                             # Another way:
+        try:                      
+            level = int(input("Level: "))   
+            if level > 0 and level <= 3:    #   if level in [1,2,3]:
+                return level                #       break
+        except ValueError:                  # except:
+            pass                            #   pass
+                                            # return level   
 def generate_integer(level):
     if level == 1:
-        x = random.randint(1,9)
-        y = random.randint(1,9)
+        x = random.randint(0,9)
+        y = random.randint(0,9)
     elif level == 2:
         x = random.randint(10,99)
         y = random.randint(10,99)
-    elif level == 3:
+    elif level == 3:                        # else:
         x = random.randint(100,999)
         y = random.randint(100,999)
-    return x
+    return x                                # return x,y
     return y
+
+def simulate_attemps(x,y):
+    attemps = 1
+    while attemps <= 3:
+        try:
+            users_answer = int(input(f"{x} + {y} = "))
+            if users_answer == (x+y):
+                return True
+            else:
+                attemps += 1
+                print("EEE")
+        except ValueError:
+            attemps += 1
+            print("EEE")    
+    print(f"{x} + {y} = {x+y}")
+    return False
+
 
 if __name__ == "__main__":
     main()
