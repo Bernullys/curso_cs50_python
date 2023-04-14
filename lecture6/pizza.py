66101c4508f81b54bf8551c20da74bdbe3edee28
badr <bernardoantoniod@gmail.com>
2023-04-14 16:45:59 -0400

    pizza completo - leer csv module

---
+++ lecture6/pizza.py
@@ -0,0 +1,31 @@
+#the program expects one command-line argument of a csv file
+
+#more than one command-line --- Too many command-line arguments
+#less than one command-line -- Too few command-line arguments
+#not a csv file -- Not a CSV file
+#if the specified file does not exist -- That file does not exist
+import csv
+import sys
+from tabulate import tabulate
+
+if len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+elif len(sys.argv) < 2:
+    sys.exit("Too few command-line arguments")
+elif sys.argv[1].endswith(".csv") == False:
+    sys.exit("Not a CSV file")
+
+menu = []
+try:
+    with open(sys.argv[1]) as csvfile:
+        reader = csv.reader(csvfile)
+        for menu_line in reader:
+            menu.append(menu_line)
+except FileNotFoundError:
+    print("File does not exist")
+
+#outputs a table with ASCII format
+
+print(tabulate(menu, headers="firstrow", tablefmt="grid"))
+
+#tabulate works with the module of csv, otherwise the impresion is not correct
