202c33d68239ddd4ea0add9495c853961378d382
badr <bernardoantoniod@gmail.com>
2023-05-04 18:49:08 -0400

    with a class

--- lecture8/student.py
+++ lecture8/student.py
@@ -1,12 +1,16 @@
+class Student:
+    ...                 #this means later will be some code here
+
 def main():
     student = get_student()
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
 
 
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return {"name": name, "house": house}
+    student = Student()     #declaring student. is like calling a function
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student
 
 
 if __name__=="__main__":
