2cdd3d2e044d50c92ad44504cadbef37932c9052
badr <bernardoantoniod@gmail.com>
2023-05-15 16:26:59 -0400

    classes jar

--- lecture8/jar.py
+++ lecture8/jar.py
@@ -2,30 +2,42 @@ class Jar:
     def __init__(self, capacity=12):
         if capacity < 0:
             raise ValueError("La capacidad no puede ser negativa")
-        #self.capacity = capacity
+        self._capacity = capacity
+        self.size = 0
     
     def __str__(self):
         return f"{self.size * '🍪'}"
 
     def deposit(self, n):
+        if n > self.capacity:
+            raise ValueError("No caben tantas galletas")
+        elif self.size + n > self.capacity:
+            raise ValueError("No caben tantas galletas")
         self.deposit = n
-        return self.deposit
+        self.size += n
 
-    def withdraw(self):
-        ...
+    def withdraw(self, withdraw):
+        self.withdraw = withdraw
+        if self.size < self.withdraw:
+            raise ValueError("No hay tantas galletas")
+        self.size -= self.withdraw
 
     @property
     def capacity(self):
-        return self.capacity
+        return self._capacity
     
-    @property
-    def size(self):
-        self.size = self.deposit
-        return self.size
+    # @property
+    # def size(self):
+    #     return self._size
 
 jarra = Jar()
-jarra.deposit(3)
-print(jarra.size())
+jarra.capacity = 3
+print(jarra.capacity)
 
-
-
+# jarra.capacity(5)
+# jarra.deposit(4)
+# print(jarra)
+# print(jarra.deposit)
+# jarra.withdraw(3)
+# print(jarra.withdraw)
+# print(jarra)
