d29824f1d090e039abbd05c01440826d55738e98
badr <bernardoantoniod@gmail.com>
2023-06-07 22:42:41 -0400

    submit plates (Ohmy it's a beauty)

--- lecture2/plates3.py
+++ lecture2/plates3.py
@@ -1,36 +1,50 @@
 def main():
+
     plate = input("Plate: ")
     if is_valid(plate):
         print("Valid")
     else:
         print("Invalid")
 
-
 def is_valid(s):
-    if len(s) > 6 or len(s) < 2:
-        return False
-    elif len(s) >= 2:
-        ese = ""
-        for letter in s[0:2]:
-            ese += letter
-        if first_two(ese) == False:
-            return False
-        if thirth_one != True:
-            return False
-        else:
-            return True
-        
-def first_two(ese):
-    if ese.isalpha():
+
+    if valid_lengh(s) and first_two(s) and third_one(s) and last_part(s):
         return True
     else:
         return False
 
-def thirth_one(ese):
-    if ese[2] == "0":
+def valid_lengh(v_l):
+
+    if len(v_l) < 2 or len(v_l) > 6:
         return False
-    else: return True
-            
+    else:
+        return True
 
+def first_two(f_t):
 
-main()
+    if len(f_t) >= 2:
+        if f_t[0:2].isalpha():
+            return True
+        else:
+            return False
+
+def third_one(t_one):
+
+    if len(t_one) >= 3:
+        if t_one[2] == "0":
+            return False
+    return True
+
+def last_part(l_part):
+    if len(l_part) >= 3:
+        for c in l_part:
+            if c.isdigit():
+                numb = l_part.index(c)
+                if l_part[numb:].isdigit():
+                    return True
+                else:
+                    return False
+    return True
+
+if __name__=="__main__":
+    main()
