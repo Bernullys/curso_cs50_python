e2f53fee7bcc84a97ec5712f5896dcbb35a3f1ca
badr <bernardoantoniod@gmail.com>
2024-12-14 19:41:24 -0300

    practicing

--- practice/daily_practice/december_eleventh.py
+++ practice/daily_practice/december_eleventh.py
@@ -1,8 +1,18 @@
-sentence = input("Enter a sentence: ")
+sentence = input("Enter a sentence: ").lower()
 
 def sep_words(texts):
     sentence_list = texts.split()
     return sentence_list
 
-list_of_words = sep_words(sentence)
-print(list_of_words)
+list_of_words = sorted(sep_words(sentence))
+
+base_dict = {}
+
+for word in list_of_words:
+    if word in base_dict:
+        base_dict[word] += 1
+    else:
+        base_dict[word] = 1
+
+for base in base_dict:
+    print(base, base_dict[base])
